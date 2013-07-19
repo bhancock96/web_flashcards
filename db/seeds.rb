931359269cdf5c9c91071250015c2c59daf23d33
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-19 15:44:10 -0500

    created user model/migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@ math_deck = Deck.create(name: "Math")
 CSV.foreach('math_deck.csv', :headers => true, :header_converters => :symbol) do |row|
   math_deck.flashcards << Flashcard.create(Hash[row])
 end
-  
+
+User.create(:name = "Priya", :username = "pri1229", :password = "123dbc")  
 
 
