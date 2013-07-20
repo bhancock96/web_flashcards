b3e9fe31bea1b60cca916feac6b9cfa5dbaf0488
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-19 21:38:55 -0500

    made next card work

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,24 +9,30 @@ get '/decks' do
   erb :decks
 end
 
-get '/decks/:id' do
-  p session
-  @deck = Deck.find(params[:id])
-  @flashcard = @deck.flashcards.first
-  @flashcards = @deck.flashcards 
+get '/decks/:deck_id/:card_id' do
+  @deck = Deck.find(params[:deck_id])
+  @flashcard = Flashcard.find(params[:card_id])
+  
   session[:answer] = @flashcard.answer
-
+  session[:flashcard_id] = @flashcard.id 
+  session[:deck_id] = @deck.id 
   erb :play_deck
 end
 
 post '/answer' do
 
   @user_answer = params[:user_answer]
+  @answer = session[:answer]
   session[:user_answer] = @user_answer
+  @flashcard_id = session[:flashcard_id]
+  @deck = session[:deck_id]
+
+  @end_of_the_deck = Deck.find(@deck).flashcards.length
 
   if @user_answer == session[:answer]
-
-    redirect '/decks/'
+    erb :good_job
+  else
+    erb :bad_job
   end
-  #compare current card answer to user_answer
+
 end
